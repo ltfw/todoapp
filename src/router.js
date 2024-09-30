b8fcffd9b0e4d7948a1119da8f8cca1c829c1aa8
Lutfi Widyanto <lutfiwidyanto18@gmail.com>
2024-09-30 09:51:00 +0700

    fix: / route

--- src/router.js
+++ src/router.js
@@ -5,10 +5,13 @@ import Register from './components/Register.vue';  // Adjust path as necessary
 import Todos from './components/Todos.vue';  // Adjust path as necessary
 
 const routes = [
-    { path: '', component: Login,name:'Login' },  // Set Login as the initial route
-    { path: '/login', component: Login,name:'Login' },
-    { path: '/register', component: Register,name:'Register' },
-    { path: '/todos', component: Todos, meta: { requiresAuth: true },name:'Todos'},
+    {
+        path: '/',
+        redirect: '/login',
+    },  // Set Login as the initial route
+    { path: '/login', component: Login, name: 'Login' },
+    { path: '/register', component: Register, name: 'Register' },
+    { path: '/todos', component: Todos, meta: { requiresAuth: true }, name: 'Todos' },
 ];
 
 const router = createRouter({
